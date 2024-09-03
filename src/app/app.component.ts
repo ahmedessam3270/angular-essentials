f6d0e8d5811f0deffc5a6920ac4c2819d1bd041f
ahmedessam201790 <ahmedessam201790@gmail.com>
2024-09-03 12:27:24 +0000

    rendering all the users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
-   DUMMY_USERS = [
-    {
-      id: 'u1',
-      name: 'Jasmine Washington',
-      avatar: 'user-1.jpg',
-    },
-    {
-      id: 'u2',
-      name: 'Emily Thompson',
-      avatar: 'user-2.jpg',
-    },
-    {
-      id: 'u3',
-      name: 'Marcus Johnson',
-      avatar: 'user-3.jpg',
-    },
-    {
-      id: 'u4',
-      name: 'David Miller',
-      avatar: 'user-4.jpg',
-    },
-    {
-      id: 'u5',
-      name: 'Priya Patel',
-      avatar: 'user-5.jpg',
-    },
-    {
-      id: 'u6',
-      name: 'Arjun Singh',
-      avatar: 'user-6.jpg',
-    },
-  ];
+  usersData= DUMMY_USERS;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -9,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+ @Input() user! : User; 
+
+ get imagePath(){
+  return 'users/' + this.user.avatar;
+ }
+
+  onSelectUser(){
+    console.log('Clicked!');
+  }
 }
