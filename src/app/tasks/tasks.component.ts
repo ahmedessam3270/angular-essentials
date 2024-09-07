a75c41175d54d17ba62567a60eec9e649ed98870
ahmedessam201790 <ahmedessam201790@gmail.com>
2024-09-07 15:50:08 +0000

    implementing showing the modal of adding new task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent , NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) name!: string;
+  isAddingTask = false;
 
    dummyTasks = [
     {
@@ -44,6 +46,10 @@ export class TasksComponent {
 
   onCompleteTask(taskId: string){
     this.dummyTasks = this.dummyTasks.filter(task => task.id !== taskId);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
   }
 
 }
